1dd9a577803eb23dc3f148915baa748f8cdd31df
Mincho Milev <mincho.milev@gmail.com>
2025-01-22 00:22:45 +0200

    Initial Commit

---
+++ hrapp/hrappApp.swift
@@ -0,0 +1,17 @@
+//
+//  hrappApp.swift
+//  hrapp
+//
+//  Created by Mincho Milev on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct hrappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
