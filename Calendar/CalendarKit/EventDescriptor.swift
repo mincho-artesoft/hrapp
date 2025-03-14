449d0662ad41143928ce95ba4ef30e9a5032b3df
Aleksandar Iliev Svinarov <47814169+JatosDetking@users.noreply.github.com>
2025-03-14 12:27:15 +0200

    ...

--- Calendar/CalendarKit/EventDescriptor.swift
+++ Calendar/CalendarKit/EventDescriptor.swift
@@ -10,17 +10,31 @@ import Foundation
 import UIKit
 
 public protocol EventDescriptor: AnyObject {
-    var dateInterval: DateInterval {get set}
-    var isAllDay: Bool {get set}
-    var text: String {get}
-    var attributedText: NSAttributedString? {get}
-    var lineBreakMode: NSLineBreakMode? {get}
-    var font : UIFont {get}
-    var color: UIColor {get}
-    var textColor: UIColor {get}
-    var backgroundColor: UIColor {get}
-    var editedEvent: EventDescriptor? {get set}
+    var dateInterval: DateInterval { get set }
+    var isAllDay: Bool { get set }
+    var text: String { get }
+    var attributedText: NSAttributedString? { get }
+    var lineBreakMode: NSLineBreakMode? { get }
+    var font: UIFont { get }
+    var color: UIColor { get }
+    var textColor: UIColor { get }
+    var backgroundColor: UIColor { get }
+    var editedEvent: EventDescriptor? { get set }
+    
+    // Добавяме calendarID тук:
+    var calendarID: String? { get }
+
     func makeEditable() -> Self
     func commitEditing()
-    
+}
+
+// Сега, в extension-а давате default реализация
+extension EventDescriptor {
+    public var calendarID: String? {
+        // Ако сте EKMultiDayWrapper:
+        if let ekWrap = self as? EKMultiDayWrapper {
+            return ekWrap.ekEvent.calendar.calendarIdentifier
+        }
+        return nil
+    }
 }
