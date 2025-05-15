964433adc6fa3a26aeec74fc30abb49ea685fa63
Aleksandar Iliev Svinarov <47814169+JatosDetking@users.noreply.github.com>
2025-05-15 12:01:26 +0300

    ...

--- Calendar/Calendar/SafariView.swift
+++ Calendar/Calendar/SafariView.swift
@@ -4,12 +4,14 @@ import SafariServices
 
 struct SafariView: UIViewControllerRepresentable {
     let url: URL
-    
+
     func makeUIViewController(context: Context) -> SFSafariViewController {
         SFSafariViewController(url: url)
     }
-    
-    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
-        // Няма нужда от обновяване
-    }
+
+    func updateUIViewController(_ controller: SFSafariViewController, context: Context) { }
+}
+
+extension URL: @retroactive Identifiable {
+    public var id: String { absoluteString }
 }
